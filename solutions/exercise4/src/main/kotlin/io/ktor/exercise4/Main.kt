446aeda56b12804745bf7dfdc5d005960c092b14
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-02-15 13:24:32 +0100

    Autoreload exercise

---
+++ solutions/exercise4/src/main/kotlin/io/ktor/exercise4/Main.kt
@@ -0,0 +1,24 @@
+package io.ktor.exercise4
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    embeddedServer(
+        Netty,
+        port = 8080,
+        watchPaths = listOf("solutions/exercise4"),
+        module = Application::mymodule
+    ).start(wait = true)
+}
+
+fun Application.mymodule() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
